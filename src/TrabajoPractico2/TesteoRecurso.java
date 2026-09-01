140e5c8d3ffb4e01aa6e5bbf8c80351386c43ce9
Jdxa <dossantosjoaquin1@gmail.com>
2026-08-31 21:42:17 -0300

    punto 1 de tp2

---
+++ src/TrabajoPractico2/TesteoRecurso.java
@@ -0,0 +1,18 @@
+package TrabajoPractico2;
+public class TesteoRecurso{
+    public static void main(String[] args) {
+        Recurso unRecurso = new Recurso();
+
+        Cliente juan = new Cliente(unRecurso);
+        juan.setName("Juan Lopez");
+        Cliente ines = new Cliente(unRecurso);
+        ines.setName("Ines Garcia");
+
+        juan.start();
+        ines.start();
+
+    }
+
+
+
+} 
