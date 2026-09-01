2fb98e08e2a689e17c23d79a6120931c8877251a
Jdxa <dossantosjoaquin1@gmail.com>
2026-09-01 17:43:35 -0300

    punto 3

--- src/TrabajoPractico2/Punto2/TestHilos.java
+++ src/TrabajoPractico2/Punto2/TestHilos.java
@@ -4,7 +4,13 @@ public class TestHilos {
     public static void main(String[] args) {
         Hilo miHilo = new Hilo();
         miHilo.start();
-        
+        try{
+            //obliga a miHilo a terminar para q continuee main
+            //fuerzo la ejecucion para que se comporte de manera deterministica
+            miHilo.join();
+        }catch (InterruptedException e){
+            System.out.println("Error");
+        }
         System.out.println("En el main");
     }
 }
