ae8edeb58943998a23ff992ff10933a1a8496099
Jdxa <dossantosjoaquin1@gmail.com>
2026-08-31 22:01:21 -0300

    comentarios sobre el punto 1

--- src/TrabajoPractico2/TesteoRecurso.java
+++ src/TrabajoPractico2/TesteoRecurso.java
@@ -8,11 +8,24 @@ public class TesteoRecurso{
         Cliente ines = new Cliente(unRecurso);
         ines.setName("Ines Garcia");
 
-        juan.start();
         ines.start();
+        juan.start();
+        
 
+        
     }
+    //hay 3 hilos: el main y Juan e Ines 
+    //el metodo uso recupera el hilo en ejecucion y muestra su nombre
+    // uso() a final del main muestra el hilo q esta en ejecucion q es el
+    //Cambiar el orden no tiene impacto, la cpu decide que hilo hace uso de su computo
+    //SALIDAS
+//soy: Juan Lopez
+// soy: Ines Garcia
+// en Recurso:  Soy Juan Lopez
+// en Recurso:  Soy Ines Garcia
 
-
-
+// soy: Ines Garcia
+// soy: Juan Lopez
+// en Recurso:  Soy Ines Garcia
+// en Recurso:  Soy Juan Lopez
 } 
