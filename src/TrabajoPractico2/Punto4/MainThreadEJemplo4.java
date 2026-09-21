de1efcd63de978550d34c6406534583c58e28b29
Jdxa <dossantosjoaquin1@gmail.com>
2026-09-21 18:15:23 -0300

    algunos cambios irrelevantes

--- src/TrabajoPractico2/Punto4/MainThreadEJemplo4.java
+++ src/TrabajoPractico2/Punto4/MainThreadEJemplo4.java
@@ -1,11 +1,9 @@
 package TrabajoPractico2.Punto4;
 public class MainThreadEJemplo4 {
     public static void main(String[] args) {
-        ThreadEjemplo alfa = new ThreadEjemplo();
-        ThreadEjemplo beta = new ThreadEjemplo();
 
-        new Thread (alfa, "Maria Jose").start();
-        new Thread (beta,"Jose Maria").start();
+        new Thread (new ThreadEjemplo(), "Maria Jose").start();
+        new Thread (new ThreadEjemplo(),"Jose Maria").start();
 
     }
 
